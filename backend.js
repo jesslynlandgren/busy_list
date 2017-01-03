37835d05985775567a355b372fad9c35b1fa4cec
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2017-01-03 09:55:30 -0500

    Fixed state transition animations on all welcome and two main index state pages.  Added front end for signup/login

--- backend.js
+++ backend.js
@@ -8,6 +8,41 @@ app.use(express.static('node_modules'));
 
 mongoose.Promise = bluebird;
 
+mongoose.connect('mongodb://localhost/getbusy');
+
+var Users = mongoose.model('User', {
+    name: {type: String, required: true, minlength: 3, maxlength: 20},
+    email: {type: String, required: true, minlength: 4, maxlength: 40, validate: /[\w\d_.]+@[a-z.]+/},
+    password: {type: String, required: true},
+    avatar: {type: String}
+});
+
+var Lists = mongoose.model('List', {
+    author_id: {type: String, required: true},
+    name: {type: String, required: true, minlength: 3, maxlength: 40, validate: /[^\t\n\r]+/},
+    created: {type: Date},
+    updated: {type: Date},
+    style: {type: String},
+    collaborators: {type: [String]}
+});
+
+var Items = mongoose.model('Item', {
+    list_id: {type: String, required: true},
+    parent_id: {type: String},
+    done: {type: Boolean, required: true},
+    created: {type: Date}
+});
+
+app.post('/signup', function(request, response){
+
+});
+
+// function auth(request, response, next){
+//
+// }
+//
+// app.use(auth);
+
 app.listen(3000, function (){
     console.log('Listing on 3000');
 });
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,5 +1,6 @@
-var app = angular.module('busy_list', ['ui.router']);
+var app = angular.module('busy_list', ['ui.router', 'ngAnimate', 'hmTouchEvents']);
 
+// Functions to handle login with Google
 function onSuccess(googleUser) {
     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
 }
@@ -20,6 +21,12 @@ function renderButton() {
 app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state({
+            name: 'index',
+            url: '/index',
+            templateUrl: 'index.html',
+            controller: 'IndexController'
+        })
+        .state({
             name: 'welcome',
             url: '/welcome',
             templateUrl: 'welcome.html',
@@ -32,44 +39,105 @@ app.config(function($stateProvider, $urlRouterProvider) {
             controller: 'AuthButtonsController'
         })
         .state({
+            name: 'welcome.signup',
+            url: '/signup',
+            templateUrl: 'signup.html',
+            controller: 'SignUpController'
+        })
+        .state({
             name: 'welcome.login',
             url: '/login',
             templateUrl: 'login.html',
             controller: 'LoginController'
         })
         .state({
-            name: 'welcome.signup',
-            url: '/signup',
-            templateUrl: 'signup.html',
-            controller: 'SignUpController'
+            name: 'index.all_lists',
+            url: '/all-lists',
+            templateUrl: 'all-lists.html',
+            controller: 'AllListsController'
+        })
+        .state({
+            name: 'index.one_list',
+            url: '/one-list',
+            templateUrl: 'one-list.html',
+            controller: 'OneListController'
         });
     $urlRouterProvider.otherwise('/welcome');
 });
 
-
-app.controller('IndexController', function($scope, busyListService, $state) {
-    $state.go('welcome');
+app.controller('IndexController', function($rootScope, $scope, busyListService, $state, $timeout) {
+    $state.go('index.all_lists');
+    $scope.className = 'all_lists';
+    $rootScope.$on('$stateChangeStart', function(event, newState) {
+        $scope.className = newState.name.split('.')[1];
+    });
 });
 
 
 app.controller('WelcomeController', function($scope, busyListService, $state) {
-    $state.go('welcome.signup');
+    $state.go('welcome.auth-buttons');
 });
 
 app.controller('AuthButtonsController', function($scope, busyListService, $state) {
 
 });
 
+app.controller('SignUpController', function($scope, busyListService, $state) {
+    $scope.signUp = function(){
+        console.log('Clicked signup button');
+        busyListService.submitSignUp()
+        .success(function(){
+            $state.go('index.all_lists');
+        })
+        .error(function(){
+            $scope.loginError = true;
+            $scope.loginMsg = 'Incorrect username or password';
+        });
+    };
+});
+
 app.controller('LoginController', function($scope, busyListService, $state) {
+    $scope.login = function(){
+        console.log('Clicked login button');
+        busyListService.submitLogin()
+        .success(function(){
+            $state.go('index.all_lists');
+        })
+        .error(function(){
+            $scope.signUpError = true;
+            $scope.signUpMsg = 'There was a problem signing up';
+        });
+    };
+});
+
+app.controller('AllListsController', function($scope, busyListService, $state) {
 
 });
 
-app.controller('SignUpController', function($scope, busyListService, $state) {
+app.controller('OneListController', function($scope, busyListService, $state) {
 
 });
 
 app.factory('busyListService', function($http, $state) {
     var service = {};
+
+    service.submitSignUp = function(data) {
+        var url = '/submit_signup';
+        return $http({
+            method: 'POST',
+            url: url,
+            data: data
+        });
+    };
+
+    service.submitLogin= function(data) {
+        var url = '/submit_login';
+        return $http({
+            method: 'POST',
+            url: url,
+            data: data
+        });
+    };
 
     return service;
 });

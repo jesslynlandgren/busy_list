5150658273564442d2f73630efca9783df0c5de7
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2017-01-11 12:57:58 -0500

    Everything up to last day of digital crafts (delete finished, mark in progress)

--- backend.js
+++ backend.js
@@ -1,13 +1,16 @@
 var express = require('express');
-var bluebird = require('bluebird');
 var mongoose = require('mongoose');
+var bluebird = require('bluebird');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+
 
 var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+app.use(bodyParser.json());
 
 mongoose.Promise = bluebird;
-
 mongoose.connect('mongodb://localhost/getbusy');
 
 var Users = mongoose.model('User', {
@@ -28,20 +31,224 @@ var Lists = mongoose.model('List', {
 
 var Items = mongoose.model('Item', {
     list_id: {type: String, required: true},
+    text: {type: String, required: true},
     parent_id: {type: String},
     done: {type: Boolean, required: true},
     created: {type: Date}
 });
 
-app.post('/signup', function(request, response){
+app.post('/submit_signup', function(request, response){
+    var name = request.body.name;
+    var email = request.body.email;
+    var password = request.body.password;
 
+    bcrypt.hash(password, 12)
+    .then(function(hash) {
+        Users.create({
+            name: name,
+            email: email,
+            password: hash
+        })
+        .then(function(userData){
+            console.log(userData);
+            Lists.create({
+                author_id: userData._id,
+                name: 'Scratch Pad',
+                created: new Date(),
+                updated: new Date()
+            })
+            .then(function(scratchPadData){
+                response.json({message:'successfully sign up', data: userData});
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+            response.status(400);
+            response.json({message:'not signed up', error: err});
+        });
+    });
 });
 
-// function auth(request, response, next){
-//
-// }
-//
-// app.use(auth);
+app.post('/submit_login', function(request, response){
+    var email = request.body.email;
+    var pwd_entered = request.body.password;
+
+    Users.find({
+        email: email
+    })
+    .then(function(users){
+        var user = users[0];
+        var pwd_hashed = user.password;
+        return bcrypt.compare(pwd_entered, pwd_hashed)
+        .then(function(allowed){
+            if (allowed){
+                response.json(user);
+            } else {
+                return false;
+            }
+        });
+    })
+    .catch(function(err){
+        response.status(401);
+        response.json({message:'invalid email or password', error: err});
+    });
+});
+
+app.get('/:user_id/all_lists', function(request, response){
+    Lists.find({
+        author_id: request.params.user_id,
+        name: {$ne: 'Scratch Pad'}
+    })
+    .then(function(lists){
+        response.json({message:'retrieved user lists', lists: lists});
+    })
+    .catch(function(err){
+        response.status(400);
+        response.json({message:'error getting lists', error: err});
+    });
+});
+
+app.post('/add_list', function(request, response){
+    Lists.create({
+        author_id: request.body.user_id,
+        name: request.body.newListTitle,
+        created: new Date(),
+        updated: new Date()
+    })
+    .then(function(data){
+        response.json({message:'list created', data: data});
+    })
+    .catch(function(err){
+        response.status(400);
+        response.json({message:'error creating list', error: err});
+    });
+});
+
+app.get('/:user_id/scratch_pad', function(request, response){
+    console.log(request.params.user_id);
+    Lists.find({
+        author_id: request.params.user_id,
+        name: 'Scratch Pad'
+    })
+    .then(function(data){
+        console.log(data);
+        Items.find({
+            list_id: data[0]._id
+        })
+        .then(function(items){
+            console.log(items);
+            response.json({message:'retrieved user lists', items: items});
+        });
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error getting list items', error: err});
+    });
+});
+
+app.post('/:user_id/scratch_pad/add', function(request, response){
+    Lists.find({
+        author_id: request.params.user_id,
+        name: 'Scratch Pad'
+    })
+    .then(function(data){
+        console.log(data);
+        Items.create({
+            list_id: data[0]._id,
+            text: request.body.newItemText,
+            done: false,
+            created: new Date()
+        })
+        .then(function(items){
+            console.log(items);
+            response.json({message:'item added to scratch pad', items: items});
+        });
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error adding item to scratch pad', error: err});
+    });
+});
+
+app.get('/:user_id/:list_id', function(request, response){
+    // console.log('USER', request.params.user_id);
+    // console.log('LIST', request.params.list_id);
+    Lists.find({
+        _id: request.params.list_id,
+    })
+    .then(function(lists){
+        Items.find({
+            list_id: lists[0]._id
+        })
+        .then(function(items){
+            response.json({message:'retrieved one list', items: items, lists: lists});
+        });
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error getting the list', error: err});
+    });
+});
+
+
+app.post('/:user_id/:list_id/add', function(request, response){
+    console.log('USER', request.params.user_id);
+    console.log('LIST', request.params.list_id);
+    Lists.find({
+        _id: request.params.list_id,
+    })
+    .then(function(lists){
+        console.log(lists);
+        Items.create({
+            list_id: lists[0]._id,
+            text: request.body.newItemText,
+            done: false,
+            created: new Date()
+        })
+        .then(function(items){
+            console.log(items);
+            response.json({message:'item added to the one list', items: items, lists: lists});
+        });
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error adding item to the one list', error: err});
+    });
+});
+
+app.post('/:item_id/mark', function(request, response){
+    console.log('DONE?', request.params.done);
+    Items.update({
+        _id: request.params.item_id,
+        done: !request.params.done
+    })
+    .then(function(items){
+        response.json({message:'item deleted from the list'});
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error deleting item', error: err});
+    });
+});
+
+app.post('/:item_id/delete', function(request, response){
+    Items.remove({
+        _id: request.params.item_id,
+    })
+    .then(function(items){
+        response.json({message:'item deleted from the list'});
+    })
+    .catch(function(err){
+        console.log('error');
+        response.status(400);
+        response.json({message:'error deleting item', error: err});
+    });
+});
 
 app.listen(3000, function (){
     console.log('Listing on 3000');
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -52,70 +52,266 @@ app.config(function($stateProvider, $urlRouterProvider) {
         })
         .state({
             name: 'index.all_lists',
-            url: '/all-lists',
+            url: '/{user_id}/all-lists',
             templateUrl: 'all-lists.html',
             controller: 'AllListsController'
         })
         .state({
+            name: 'index.scratch_pad',
+            url: '/{user_id}/scratch-pad',
+            templateUrl: 'scratch-pad.html',
+            controller: 'ScratchPadController'
+        })
+        .state({
             name: 'index.one_list',
-            url: '/one-list',
+            url: '/{user_id}/{list_id}',
             templateUrl: 'one-list.html',
             controller: 'OneListController'
         });
-    $urlRouterProvider.otherwise('/welcome');
+    $urlRouterProvider.otherwise('/welcome/auth-buttons');
 });
 
-app.controller('IndexController', function($rootScope, $scope, busyListService, $state, $timeout) {
-    $state.go('index.all_lists');
-    $scope.className = 'all_lists';
-    $rootScope.$on('$stateChangeStart', function(event, newState) {
-        $scope.className = newState.name.split('.')[1];
+app.controller('IndexController', function($rootScope, $scope, busyListService, $state, $timeout, $urlRouter) {
+    $rootScope.$on('$stateChangeStart', function(event, newState, newParams, oldState, oldParams) {
+        console.log('Transition from:', oldState.name);
+        console.log('Transition to:', newState.name);
+        if ((oldState.name === 'welcome.login' || oldState.name === 'welcome.signup') && (newState.name === 'index.all_lists')){
+            $rootScope.className = 'loggingin';
+            console.log($rootScope.className);
+            $timeout(function(){
+                $rootScope.className = 'all_lists';
+                console.log($rootScope.className);
+            },2000);
+        } else if ((oldState.name === 'index.all_lists') && (newState.name === 'welcome.login')) {
+            event.preventDefault();
+            $rootScope.className = 'loggingout';
+            console.log($rootScope.className);
+            $timeout(function(){
+                $state.go(newState.name, {}, {notify: false}).then(function() {
+                    $rootScope.$broadcast('$stateChangeSuccess', newState, newParams, oldState, oldParams);
+                });
+            }, 500);
+        } else {
+            $rootScope.className = newState.name.split('.')[1];
+            console.log($rootScope.className);
+        }
     });
 });
 
-
-app.controller('WelcomeController', function($scope, busyListService, $state) {
-    $state.go('welcome.auth-buttons');
+app.controller('WelcomeController', function($rootScope, $scope, busyListService, $state, $timeout, $urlRouter) {
+    // $state.go('welcome.login');
+    $rootScope.$on('$stateChangeStart', function(event, newState, newParams, oldState, oldParams) {
+        console.log('Transition from:', oldState.name);
+        console.log('Transition to:', newState.name);
+        if ((oldState.name === 'welcome.login' || oldState.name === 'welcome.signup') && (newState.name === 'index.all_lists')){
+            $rootScope.className = 'loggingin';
+            console.log($rootScope.className);
+            $timeout(function(){
+                $rootScope.className = 'all_lists';
+                console.log($rootScope.className);
+            },2000);
+        } else if ((oldState.name === 'index.all_lists') && (newState.name === 'welcome.login')) {
+            event.preventDefault();
+            $rootScope.className = 'loggingout';
+            console.log($rootScope.className);
+            $timeout(function(){
+                $state.go(newState.name, {}, {notify: false}).then(function() {
+                    $rootScope.$broadcast('$stateChangeSuccess', newState, newParams, oldState, oldParams);
+                });
+            }, 500);
+        } else {
+            $rootScope.className = newState.name.split('.')[1];
+            console.log($rootScope.className);
+        }
+    });
 });
 
-app.controller('AuthButtonsController', function($scope, busyListService, $state) {
+app.controller('AuthButtonsController', function($rootScope, $scope, busyListService, $state) {
 
 });
 
-app.controller('SignUpController', function($scope, busyListService, $state) {
+app.controller('SignUpController', function($rootScope, $scope, busyListService, $state) {
+    $scope.signUpForm = {};
     $scope.signUp = function(){
         console.log('Clicked signup button');
-        busyListService.submitSignUp()
-        .success(function(){
-            $state.go('index.all_lists');
+        busyListService.submitSignUp($scope.signUpForm)
+        .then(function(result){
+            console.log(result);
+            $state.go('index.all_lists', {user_id: result.data.data._id});
         })
-        .error(function(){
-            $scope.loginError = true;
-            $scope.loginMsg = 'Incorrect username or password';
-        });
-    };
-});
-
-app.controller('LoginController', function($scope, busyListService, $state) {
-    $scope.login = function(){
-        console.log('Clicked login button');
-        busyListService.submitLogin()
-        .success(function(){
-            $state.go('index.all_lists');
-        })
-        .error(function(){
+        .catch(function(result){
+            console.log(result);
             $scope.signUpError = true;
             $scope.signUpMsg = 'There was a problem signing up';
         });
     };
 });
 
-app.controller('AllListsController', function($scope, busyListService, $state) {
-
+app.controller('LoginController', function($rootScope, $scope, busyListService, $state) {
+    $scope.loginForm = {};
+    $scope.login = function(){
+        console.log('Clicked login button');
+        busyListService.submitLogin($scope.loginForm)
+        .then(function(result){
+            console.log(result);
+            $state.go('index.all_lists', {user_id: result.data._id});
+        })
+        .catch(function(result){
+            console.log(result);
+            $scope.loginError = true;
+            $scope.loginMsg = 'Incorrect username or password';
+        });
+    };
 });
 
-app.controller('OneListController', function($scope, busyListService, $state) {
+app.controller('AllListsController', function($rootScope, $scope, busyListService, $state, $stateParams) {
+    // console.log("In ALL LISTS");
+    $scope.showSettings = false;
+    $scope.addingList = false;
+    $rootScope.user_id = $stateParams.user_id;
 
+    $scope.toggleSettings = function(){
+        $scope.showSettings = !$scope.showSettings;
+    };
+    $scope.logout = function() {
+        $state.go('welcome.login');
+    };
+
+    busyListService.getAllLists($rootScope.user_id)
+    .then(function(result){
+        $scope.lists = result.data.lists;
+    });
+
+    $scope.toggleNewList = function(){
+        $scope.addingList = !$scope.addingList;
+    };
+
+    $scope.addList = function(){
+        if ($scope.newListTitle && $scope.newListTitle.length > 2) {
+            busyListService.addList($rootScope.user_id, $scope.newListTitle)
+            .then(function(result){
+                console.log('ADDED NEW LIST');
+                console.log(result);
+                console.log($rootScope.user_id);
+                $scope.addingList = false;
+                busyListService.getAllLists($rootScope.user_id)
+                .then(function(result){
+                    console.log('Lists:', result);
+                    $scope.lists = result.data.lists;
+                });
+                $state.go('index.all_lists', {user_id: $rootScope.user_id});
+                // state.go('one_list', {list_id: result.data._id});
+            })
+            .catch(function(result){
+                console.log(result);
+            });
+        } else {
+            $scope.message = 'Give your new list a name!';
+        }
+    };
+});
+
+app.controller('ScratchPadController', function($rootScope, $scope, busyListService, $state, $stateParams) {
+    $scope.addingItem = false;
+    $rootScope.user_id = $stateParams.user_id;
+
+    busyListService.getScratchPad($rootScope.user_id)
+    .then(function(result){
+        $scope.items = result.data.items;
+    });
+
+    $scope.toggleNewItem = function(){
+        $scope.addingItem = !$scope.addingItem;
+    };
+
+    $scope.addItem = function(){
+        if ($scope.newItemText) {
+            busyListService.addItemScratchPad($rootScope.user_id, $scope.newItemText)
+            .then(function(result){
+                $scope.newItemText = "";
+                $scope.addingItem = false;
+                busyListService.getScratchPad($rootScope.user_id)
+                .then(function(result){
+                    $scope.items = result.data.items;
+                });
+            })
+            .catch(function(result){
+                console.log(result);
+            });
+        } else {
+            $scope.message = "You can't add an empty item!";
+        }
+    };
+});
+
+app.controller('OneListController', function($rootScope, $scope, busyListService, $state, $stateParams) {
+    $scope.addingItem = false;
+    $rootScope.user_id = $stateParams.user_id;
+    $scope.list_id = $stateParams.list_id;
+
+    busyListService.getListItems($rootScope.user_id, $scope.list_id)
+    .then(function(result){
+        console.log(result);
+        $scope.list = result.data.lists[0];
+        $scope.items = result.data.items;
+    });
+
+    $scope.toggleNewItem = function(){
+        $scope.addingItem = !$scope.addingItem;
+    };
+
+    $scope.addItem = function(){
+        console.log($scope.newItemText);
+        if ($scope.newItemText) {
+            busyListService.addItem($rootScope.user_id, $scope.list_id, $scope.newItemText)
+            .then(function(result){
+                console.log('ADDED NEW ITEM');
+                console.log(result);
+                console.log($rootScope.user_id);
+                $scope.newItemText = "";
+                $scope.addingItem = false;
+                busyListService.getListItems($rootScope.user_id, $scope.list_id)
+                .then(function(result){
+                    $scope.items = result.data.items;
+                });
+            })
+            .catch(function(result){
+                console.log(result);
+            });
+        } else {
+            $scope.message = "You can't add an empty item!";
+        }
+    };
+
+    $scope.markItem = function(item_id, done) {
+        console.log(item_id);
+        console.log(done);
+        busyListService.markItem(item_id, done)
+        .then(function(result) {
+            console.log('MARKED ITEM');
+            busyListService.getListItems($rootScope.user_id, $scope.list_id)
+            .then(function(result){
+                $scope.items = result.data.items;
+            });
+        })
+        .catch(function(result){
+            console.log(result);
+        });
+    };
+
+    $scope.deleteItem = function(item_id) {
+        busyListService.deleteItem(item_id)
+        .then(function(result) {
+            console.log('DELETED ITEM');
+            busyListService.getListItems($rootScope.user_id, $scope.list_id)
+            .then(function(result){
+                $scope.items = result.data.items;
+            });
+        })
+        .catch(function(result){
+            console.log(result);
+        });
+    };
 });
 
 app.factory('busyListService', function($http, $state) {
@@ -130,7 +326,7 @@ app.factory('busyListService', function($http, $state) {
         });
     };
 
-    service.submitLogin= function(data) {
+    service.submitLogin = function(data) {
         var url = '/submit_login';
         return $http({
             method: 'POST',
@@ -138,6 +334,168 @@ app.factory('busyListService', function($http, $state) {
             data: data
         });
     };
+
+    service.getAllLists = function(user_id) {
+        var url = '/' + user_id + '/all_lists';
+        return $http({
+            method: 'GET',
+            url: url,
+            params: {
+                user_id: user_id
+            }
+        });
+    };
+
+    service.addList = function(user_id, newListTitle) {
+        var url = '/add_list';
+        return $http({
+            method: 'POST',
+            url: url,
+            data: {
+                user_id: user_id,
+                newListTitle: newListTitle
+            }
+        });
+    };
+
+    service.getScratchPad = function(user_id) {
+        var url = '/' + user_id + '/scratch_pad';
+        return $http({
+            method: 'GET',
+            url: url,
+            params: {
+                user_id: user_id
+            }
+        });
+    };
+
+    service.addItemScratchPad = function(user_id, newItemText) {
+        var url = '/' + user_id + '/scratch_pad/add';
+        return $http({
+            method: 'POST',
+            url: url,
+            params: {
+                user_id: user_id
+            },
+            data: {
+                newItemText: newItemText
+            }
+        });
+    };
+
+    service.getListItems = function(user_id, list_id) {
+        var url = '/' + user_id + '/' + list_id;
+        return $http({
+            method: 'GET',
+            url: url,
+            params: {
+                user_id: user_id,
+                list_id: list_id
+            }
+        });
+    };
+
+    service.addItem = function(user_id, list_id, newItemText) {
+        var url = '/' + user_id + '/' + list_id + '/add';
+        return $http({
+            method: 'POST',
+            url: url,
+            params: {
+                user_id: user_id,
+                list_id: list_id
+            },
+            data: {
+                newItemText: newItemText
+            }
+        });
+    };
+
+    service.markItem = function(item_id, done) {
+        var url = '/' + item_id + '/mark';
+        return $http({
+            method: 'POST',
+            url: url,
+            params: {
+                item_id: item_id,
+                done: done
+            }
+        });
+    };
+
+    service.deleteItem = function(item_id) {
+        var url = '/' + item_id + '/delete';
+        return $http({
+            method: 'POST',
+            url: url,
+            params: {
+                item_id: item_id
+            }
+        });
+    };
+
+    // service.deleteList = function(user_id) {
+    //     var url = '/add_list';
+    //     return $http({
+    //         method: 'POST',
+    //         url: url,
+    //     });
+    // };
+    //
+    // service.copyList = function(user_id) {
+    //     var url = '/add_list';
+    //     return $http({
+    //         method: 'POST',
+    //         url: url,
+    //     });
+    // };
+
+    // service.getScratchPad = function() {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
+    //
+    // service.getOneList = function(list_id) {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
+    //
+    // service.addItem = function(list_id) {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
+
+    // service.updateItem = function(list_id) {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
+    //
+    // service.deleteItem = function(list_id) {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
+    //
+    // service.copyItem = function(list_id) {
+    //     var url = '/scratch_pad';
+    //     return $http({
+    //         method: 'GET',
+    //         url: url,
+    //     });
+    // };
 
     return service;
 });
